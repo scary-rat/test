e7b24c9dc737b73e29710d1f2f6d9b489e405a26
rocks <verceldeploys@proton.me>
2024-10-10 19:39:51 +0530

    completed

--- git-repo/repo.js
+++ git-repo/repo.js
@@ -1,4 +1,11 @@
 // commands
 
-// git remote add origin https://github.com/scary-rat/test         // git hub ko repo ko origin set greko
-// git push origin 
+// git remote add origin https://github.com/scary-rat/test     // git hub ko repo ko origin set greko
+
+// git push -u origin main                                     // github ko main branch ma push garni current changes
+
+// git pull origin main                                        // github ko main branch lie pull gara
+
+// git switch -C learning                                      // yo chai -C vbneko yi branch already exist xaina vani create gara ani switch gara
+
+// git fetch                                                   // it fetches all the updates like new branches, new tags from repo
